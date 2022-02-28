13ee4ff51c6e827d9c377cdd58ea38512f3298c3
tolimpo <olimpo.o@northeastern.edu>
2022-02-27 22:43:38 -0500

    Initial commit 2

--- src/cs3500/freecell/model/multimove/MultimoveCascadePile.java
+++ src/cs3500/freecell/model/multimove/MultimoveCascadePile.java
@@ -1,9 +1,10 @@
 package cs3500.freecell.model.multimove;
 
-import cs3500.freecell.model.AbstractPile;
 import cs3500.freecell.model.Card;
+import cs3500.freecell.model.CascadePile;
 
-public class MultimoveCascadePile extends AbstractPile {
+public class MultimoveCascadePile extends CascadePile {
+
   @Override
   protected boolean validMove(Card mover, Card target) {
     return false;
