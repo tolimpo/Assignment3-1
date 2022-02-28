53302be1c94464536f0128c8fb8c7ce7d6d2bd44
tolimpo <olimpo.o@northeastern.edu>
2022-02-28 18:48:57 -0500

    Runs multimove instead of simple

--- src/cs3500/freecell/Main.java
+++ src/cs3500/freecell/Main.java
@@ -7,6 +7,7 @@ import cs3500.freecell.controller.SimpleFreecellController;
 import cs3500.freecell.model.Card;
 import cs3500.freecell.model.FreecellModel;
 import cs3500.freecell.model.SimpleFreecellModel;
+import cs3500.freecell.model.multimove.MultimoveModel;
 
 /**
  * Main class to run the code for testing.
@@ -18,7 +19,7 @@ public class Main {
    * @param args possible initial inputs
    */
   public static void main(String[] args) {
-    FreecellModel<Card> model = new SimpleFreecellModel();
+    FreecellModel<Card> model = new MultimoveModel();
     FreecellController<Card> ctrl = new SimpleFreecellController(model,
         new InputStreamReader(System.in), System.out);
     ctrl.playGame(model.getDeck(), 8, 4, false);
