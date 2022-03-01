be65dca8d16848d060e1ca979fbb5ce9b974ac6e
tolimpo <olimpo.o@northeastern.edu>
2022-03-01 18:04:10 -0500

    Tests complete

--- test/MultimoveModelTest.java
+++ test/MultimoveModelTest.java
@@ -1,11 +1,13 @@
 import org.junit.Test;
 
 import cs3500.freecell.model.Card;
+import cs3500.freecell.model.CascadePile;
 import cs3500.freecell.model.FreecellModel;
 import cs3500.freecell.model.FreecellModelCreator;
 import cs3500.freecell.model.PileType;
 import cs3500.freecell.model.Rank;
 import cs3500.freecell.model.Suit;
+import cs3500.freecell.view.FreecellTextView;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,6 +20,7 @@ public class MultimoveModelTest extends AbstractModelTest {
 
   @Test
   public void testMoveValidBuild() {
+    // test setup
     Card aceClubs = new Card(Suit.CLUBS, Rank.ACE);
     assertEquals(aceClubs, game2unshuffled.getCascadeCardAt(0, 6));
     game2unshuffled.move(PileType.CASCADE, 0, 6, PileType.CASCADE, 5);
@@ -56,13 +59,128 @@ public class MultimoveModelTest extends AbstractModelTest {
 
   @Test(expected = IllegalArgumentException.class)
   public void testMoveInvalidBuild() {
-    // putting others into open piles so that the destination card
-    // is correct, since this just testing an invalid build
+    // test setup
     game2unshuffled.move(PileType.CASCADE, 3, 6, PileType.OPEN, 0);
     game2unshuffled.move(PileType.CASCADE, 3, 5, PileType.OPEN, 1);
 
-    // exception thrown here
+    // exception thrown here, destination card works, but the build is invalid
     game2unshuffled.move(PileType.CASCADE, 5, 4, PileType.CASCADE, 3);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testMoveGoodBuildButCannotGoOnDestination() {
+    // test setup
+    game2unshuffled.move(PileType.CASCADE, 0, 6, PileType.CASCADE, 5);
+
+    // exception thrown here, build is valid, but destination card forbids it
+    game2unshuffled.move(PileType.CASCADE, 5, 5, PileType.CASCADE, 4);
+
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testBuildOverMaxLimitNoEmptyCascade() {
+    // test setup
+    game2unshuffled.move(PileType.CASCADE, 0, 6, PileType.CASCADE, 5);
+    game2unshuffled.move(PileType.CASCADE, 5, 5, PileType.CASCADE, 0);
+    game2unshuffled.move(PileType.CASCADE, 0, 5, PileType.CASCADE, 5);
+    game2unshuffled.move(PileType.CASCADE, 5, 4, PileType.CASCADE, 0);
+    game2unshuffled.move(PileType.CASCADE, 0, 4, PileType.CASCADE, 5);
+
+    // exception thrown here, trying to move a build of 6 card
+    game2unshuffled.move(PileType.CASCADE, 5, 3, PileType.CASCADE, 0);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testBuildOverMaxLimitOneEmptyCascade() {
+    // test setup (to get one empty cascade)
+    int currSourcePile = 3;
+    int currCardIndex = 6;
+    int destPile = 0;
+    while (game2unshuffled.getNumCardsInCascadePile(7) != 0) {
+      game2unshuffled.move(PileType.CASCADE,
+          currSourcePile, currCardIndex, PileType.FOUNDATION, destPile);
+      if (currSourcePile == 3) {
+        currSourcePile = 7;
+        currCardIndex--;
+      } else {
+        currSourcePile = 3;
+      }
+    }
+
+    // filling up open piles
+    for (int i = 6; i > 2; i--) {
+      game2unshuffled.move(PileType.CASCADE, 0, i, PileType.OPEN, i - 3);
+    }
+
+    // moving to create a build...
+    game2unshuffled.move(PileType.CASCADE, 1, 6, PileType.CASCADE, 4);
+    // moving a build of 2 cards (maximum in this case)
+    game2unshuffled.move(PileType.CASCADE, 4, 5, PileType.CASCADE, 1);
+
+    // exception thrown here, trying to move build of 3 cards
+    game2unshuffled.move(PileType.CASCADE, 1, 5, PileType.CASCADE, 4);
+
+  }
+
+  @Test
+  public void testMoveBuildToEmptyCascade() {
+    // test setup (to get one empty cascade)
+    int currSourcePile = 3;
+    int currCardIndex = 6;
+    int destPile = 0;
+    while (game2unshuffled.getNumCardsInCascadePile(7) != 0) {
+      game2unshuffled.move(PileType.CASCADE,
+          currSourcePile, currCardIndex, PileType.FOUNDATION, destPile);
+      if (currSourcePile == 3) {
+        currSourcePile = 7;
+        currCardIndex--;
+      } else {
+        currSourcePile = 3;
+      }
+    }
+
+
+    Card aceDiamonds = new Card(Suit.DIAMONDS, Rank.ACE);
+    assertEquals(aceDiamonds, game2unshuffled.getCascadeCardAt(1, 6));
+    game2unshuffled.move(PileType.CASCADE, 1, 6, PileType.CASCADE, 4);
+    assertEquals(aceDiamonds, game2unshuffled.getCascadeCardAt(4, 6));
+
+    Card twoClubs = new Card(Suit.CLUBS, Rank.TWO);
+    assertEquals(twoClubs, game2unshuffled.getCascadeCardAt(4, 5));
+    game2unshuffled.move(PileType.CASCADE, 4, 5, PileType.CASCADE, 7);
+    assertEquals(twoClubs, game2unshuffled.getCascadeCardAt(7, 0));
+    assertEquals(aceDiamonds, game2unshuffled.getCascadeCardAt(7, 1));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testMoveBuildToEmptyCascadeNuance() {
+    // test setup (to get one empty cascade)
+    int currSourcePile = 3;
+    int currCardIndex = 6;
+    int destPile = 0;
+    while (game2unshuffled.getNumCardsInCascadePile(7) != 0) {
+      game2unshuffled.move(PileType.CASCADE,
+          currSourcePile, currCardIndex, PileType.FOUNDATION, destPile);
+      if (currSourcePile == 3) {
+        currSourcePile = 7;
+        currCardIndex--;
+      } else {
+        currSourcePile = 3;
+      }
+    }
+
+    // moving to create a build...
+    game2unshuffled.move(PileType.CASCADE, 0, 6, PileType.CASCADE, 5);
+    game2unshuffled.move(PileType.CASCADE, 5, 5, PileType.CASCADE, 0);
+    game2unshuffled.move(PileType.CASCADE, 0, 5, PileType.CASCADE, 5);
+    // moving a build of 4, which is the max because the pile will be moved to an empty cascade
+    game2unshuffled.move(PileType.CASCADE, 5, 4, PileType.CASCADE, 0);
+
+    /*
+    Exception thrown here, testing the nuance of when the move is to
+    an empty cascade pile, the destination pile does not count as empty.
+     */
+    game2unshuffled.move(PileType.CASCADE, 0, 4, PileType.CASCADE, 7);
   }
 
 }
