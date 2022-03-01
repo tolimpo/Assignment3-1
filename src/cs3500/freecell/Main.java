5f7a660b0623665193ddb87d69c1666da47cabb8
tolimpo <olimpo.o@northeastern.edu>
2022-02-28 21:05:32 -0500

    changed to use creator class

--- src/cs3500/freecell/Main.java
+++ src/cs3500/freecell/Main.java
@@ -6,8 +6,7 @@ import cs3500.freecell.controller.FreecellController;
 import cs3500.freecell.controller.SimpleFreecellController;
 import cs3500.freecell.model.Card;
 import cs3500.freecell.model.FreecellModel;
-import cs3500.freecell.model.SimpleFreecellModel;
-import cs3500.freecell.model.multimove.MultimoveModel;
+import cs3500.freecell.model.FreecellModelCreator;
 
 /**
  * Main class to run the code for testing.
@@ -16,10 +15,12 @@ public class Main {
 
   /**
    * Main method to run the code for testing.
+   *
    * @param args possible initial inputs
    */
   public static void main(String[] args) {
-    FreecellModel<Card> model = new MultimoveModel();
+    FreecellModel<Card> model = FreecellModelCreator
+        .create(FreecellModelCreator.GameType.MULTIMOVE);
     FreecellController<Card> ctrl = new SimpleFreecellController(model,
         new InputStreamReader(System.in), System.out);
     ctrl.playGame(model.getDeck(), 8, 4, false);
